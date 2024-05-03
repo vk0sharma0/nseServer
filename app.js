43720578aa769cbd5059388cf3c2c0c9caaeaaad
vk0sharma0 <136582084+vk0sharma0@users.noreply.github.com>
2024-05-03 11:41:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+import axios from 'axios';
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
@@ -98,7 +100,13 @@ async function myfun() {
             }
         });*/
         
-         const response = await fetch("https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY");
+         const response = await fetch("https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY",{headers: {
+            'User-Agent': 'Mozilla/5.0',
+            'Accept': 'application/json ,text/plain' ,
+             'Accept-Encoding' : 'gzip, compress, deflate' 
+            
+    }
+        });
          const data = await response.text();
         
         
